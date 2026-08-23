61ea07496e6f25a9004e30717e415246c48617d8
Neriomanuel03 <neriomanuel03gmail.com>
2026-08-23 14:28:57 +0100

    Todo codigo

---
+++ script.js
@@ -0,0 +1,32 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from("#hero", {
+  opacity: 0,
+  y: 60,
+  duration: 3,
+  ease: "power3.out"
+});
+
+gsap.from("#sobre", {
+  scrollTrigger: {
+    trigger: ".sobre",
+    start: "top 80%"
+  },
+  opacity: 0,
+  y: 80,
+  duration: 4,
+  ease: "power3.out"
+});
+
+gsap.from(".contacto", {
+  scrollTrigger: {
+    trigger: ".projectos",
+    start: "top 80%"
+  },
+  opacity: 0,
+  y: 80,
+  duration: 4,
+  ease: "power3.out"
+});
+
+
