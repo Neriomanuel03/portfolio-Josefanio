fcd840e3525b933f8f89b2b2ff13f02ede9399aa
Neriomanuel03 <neriomanuel03@gmail.com>
2026-09-06 23:39:45 +0100

    Seccao formacao

--- teste.js
+++ teste.js
@@ -1,5 +1,19 @@
+/*
 function enviar()
 {
     var valueInput = document.getElementById('valor').value;
     alert(valueInput)
-}
+}*/
+
+/* let nome ="Nerio";
+let idade=23;
+console.log("Ola o teu nome é " +nome + " e tens " +idade + "anos.");
+
+console.log(typeof nome); */
+
+const numero = prompt("Digite um numero: ");
+    if (numero % 2 === 0){
+        console.log("O numero é par");
+    } else{
+    console.log("impar")
+   }
