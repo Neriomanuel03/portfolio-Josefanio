65fb61ac98f544d49515f1b074328c7ac87318e8
Neriomanuel03 <neriomanuel03@gmail.com>
2026-09-09 02:57:22 +0100

    js teste

--- teste.js
+++ teste.js
@@ -1,4 +1,5 @@
-/*
+/* dando funcionalidade ao botao
+ *//*
 function enviar()
 {
     var valueInput = document.getElementById('valor').value;
@@ -11,9 +12,32 @@ console.log("Ola o teu nome é " +nome + " e tens " +idade + "anos.");
 
 console.log(typeof nome); */
 
-const numero = Number (prompt("Digite um numero: "));
+/* o resto da divisao
+ *//* const numero = Number (prompt("Digite um numero: "));
     if (numero % 2 === 0){
         console.log("O numero é par");
     } else{
     console.log("impar")
-   }
+   } 
+
+  /*   dobro de um numero
+    let numero = Number(prompt("Digite um número:"));
+let dobro = numero * 2;
+
+console.log(dobro); */
+
+/* 4 operações */
+let n_1 = Number (prompt("Digite o primeiro numero: "));
+let n_2 = Number (prompt("Digite o segundo numero: "));
+let soma = (n_1 + n_2);
+let sub= (n_1 - n_2);
+let time= (n_1 * n_2);
+let div= (n_1 / n_2);
+console.log("O primeiro numero é: " +n_1);
+console.log("O segundo numero é: " +n_2);
+
+console.log("soma é: " +soma);
+console.log("Subbtração: " +sub);
+console.log("Multiplicacao: " +time);
+console.log("Divisão: " +div);
+
